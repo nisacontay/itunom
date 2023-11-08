84800422f5b7bd054274037c4000e61deafb5ef6
nisacontay <148093015+nisacontay@users.noreply.github.com>
2023-11-08 04:31:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,91 @@
+from ultralytics import YOLO
+import cv2
+import math 
+# start webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+# model
+model = YOLO("yolo-Weights/yolov8n.pt")
+
+# object classes
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+
+while True:
+    success, img = cap.read()
+    results = model(img, stream=True)
+
+    #kalıcı kutu
+    height, width, _ = img.shape
+    print(height, width)
+    h2= int(height * 0.10)
+    h1 = int(height * 0.90)
+    w1 = int(width * 0.25)
+    w2 = int(width * 0.75)
+    print(h1, h2, w1, w2)
+
+    cv2.rectangle(img, (w1, h1), (w2, h2), (128, 0, 0), 2)
+    
+    #cv2.imshow("Webcam", img)
+
+            # put box in cam
+    for r in results:
+        boxes = r.boxes
+       
+        for box in boxes:
+            # bounding box
+                 # coordinates
+            x1, x2 , y1 , y2 = box.xyxy[0]
+            # convert to int value
+            x1, x2 , y1, y2 = int(x1), int(x2), int(y1), int(y2)
+            print(x1, x2, y1, y2)
+            if x2 < w2 and x1 < w2 and y2 < h2 and y1 < h2:
+                break
+            elif x1 > w1 and x2 > w1 and y1 > h1 and y2 > h1:
+                break
+
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 2)
+
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+            print("Confidence --->",confidence)
+
+            # class name
+            cls = int(box.cls[0])
+            print("Class name -->", classNames[cls])
+
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+
+            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+
+            w1 = x1 * 0.25
+            h1 = y1 * 0.10
+            w2 = x2 * 0.75
+            h2 = y2 * 0.90
+
+            #cv2.rectangle(img, (w1, h2), (w2, h2),(128, 0, 0), 3 )
+
+
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
